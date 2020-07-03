997c3d12291e984b14de2c9afeb4575ff9f28948
olivier.bonheur <olivier.bonheur@gmail.com>
2020-07-03 23:41:40 +0200

    Add JavaDoc for the repository factory

--- PayMyBuddy/src/main/java/com/paymybuddy/factory/RepositoryFactoryNullVersion.java
+++ PayMyBuddy/src/main/java/com/paymybuddy/factory/RepositoryFactoryNullVersion.java
@@ -11,38 +11,26 @@ import com.paymybuddy.repository.TransactionRepositoryJpaImpl;
 import com.paymybuddy.repository.UtilisateurRepositoryJdbcImpl;
 import com.paymybuddy.repository.UtilisateurRepositoryJpaImpl;
 
+/**
+ * Class Factory in charge of the construction of Utilisateur repository and
+ * Transaction repository
+ */
 public class RepositoryFactoryNullVersion {
-
-	private static ITransactionRepository transactionRepository = null;
-
-	public static ITransactionRepository getTransactionRepository(String repositoryName, String properties) {
-
-		if (repositoryName.equals("jdbc")) {
-			if (transactionRepository == null) {
-
-				RepositoryJdbcConfiguration repositoryConfiguration = RepositoryJdbcConfiguration
-						.getRepositoryConfiguration(properties);
-
-				transactionRepository = new TransactionRepositoryJdbcImpl(repositoryConfiguration);
-
-			}
-			return transactionRepository;
-		}
-
-		if (repositoryName.equals("jpa")) {
-			if (transactionRepository == null) {
-
-				EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(properties);
-
-				transactionRepository = new TransactionRepositoryJpaImpl(entityManagerFactory);
-
-			}
-		}
-		return transactionRepository;
-	}
 
 	private static IUtilisateurRepository utilisateurRepository = null;
 
+	private static ITransactionRepository transactionRepository = null;
+
+	/**
+	 * Create a Utilisateur repository.
+	 * 
+	 * @param repositoryName The name of the repository to create
+	 * 
+	 * @param properties     The path of the file containing properties for the
+	 *                       repository configuration
+	 * 
+	 * @return The Utilisateur repository
+	 */
 	public static IUtilisateurRepository getUtilisateurRepository(String repositoryName, String properties) {
 
 		if (repositoryName.equals("jdbc")) {
@@ -67,6 +55,42 @@ public class RepositoryFactoryNullVersion {
 			}
 		}
 		return utilisateurRepository;
+	}
+
+	/**
+	 * Create a Transaction repository.
+	 * 
+	 * @param repositoryName The name of the repository to create
+	 * 
+	 * @param properties     The path of the file containing properties for the
+	 *                       repository configuration
+	 * 
+	 * @return The Transaction repository
+	 */
+	public static ITransactionRepository getTransactionRepository(String repositoryName, String properties) {
+
+		if (repositoryName.equals("jdbc")) {
+			if (transactionRepository == null) {
+
+				RepositoryJdbcConfiguration repositoryConfiguration = RepositoryJdbcConfiguration
+						.getRepositoryConfiguration(properties);
+
+				transactionRepository = new TransactionRepositoryJdbcImpl(repositoryConfiguration);
+
+			}
+			return transactionRepository;
+		}
+
+		if (repositoryName.equals("jpa")) {
+			if (transactionRepository == null) {
+
+				EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(properties);
+
+				transactionRepository = new TransactionRepositoryJpaImpl(entityManagerFactory);
+
+			}
+		}
+		return transactionRepository;
 	}
 
 }
